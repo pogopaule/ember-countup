5c5b588a70a47e8cf6fae974e3da56a4e65a60d3
pogopaule <pogopaule@gmx.de>
2015-08-26 23:36:36 +0200

    add countUp.js dependency

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-countup'
+  name: 'ember-countup',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/countUp.js/dist/countUp.js');
+  }
 };

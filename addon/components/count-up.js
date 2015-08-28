f56230059c93181fb0916215cb2ff91cf24e4816
pogopaule <pogopaule@gmx.de>
2015-08-28 20:37:40 +0200

    add count-up component

---
+++ addon/components/count-up.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['ember-countup'],
+  tagName: 'span',
+
+  didInsertElement() {
+    this._insertAndStartCountUp();
+  },
+
+  changed: Ember.observer('startVal', 'endVal', 'decimals', 'duration', 'useEasing', 'useGrouping', 'separator', 'decimal', 'prefix', 'suffix', function() {
+    this._insertAndStartCountUp();
+  }),
+
+  _insertAndStartCountUp() {
+    Ember.run.next(() => {
+      new CountUp(
+        this.get('elementId'),
+        this.get('startVal') || 0,
+        this.get('endVal') || 42,
+        this.get('decimals') || 0,
+        this.get('duration') || 2,
+        {
+          useEasing: this.get('useEasing'),
+          useGrouping: this.get('useGrouping'),
+          separator: this.get('separator') || ',',
+          decimal: this.get('decimal') || '.',
+          prefix: this.get('prefix') || '',
+          suffix: this.get('suffix') || ''
+        }
+      ).start();
+    });
+  }
+});
+
---
+++ app/components/count-up.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-countup/components/count-up';
